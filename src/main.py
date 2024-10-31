4e8a2011143a987f91d5277f5bb6513e58f7d0b4
Ephraim67 <jonathaneuphrem@gmail.com>
2024-10-31 12:33:19 +0100

    new changes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
 # Main Python Scripts
+import json
+
+def load_broker_config():
+    with open("config/broker_config.json") as f:
+        return json.load(f)
+    
+def load_bot_settings():
+    with open("config/settings.json") as f:
+        return json.load(f)
+    
+broker_config = load_broker_config()
+bot_settings = load_bot_settings()
